615e65d3c21dadb7ca366ca058e1ea9872942197
Jackson Burns <jwburns@mit.edu>
2024-01-11 13:15:16 -0500

    clean up example directory

--- examples/track_dropped.py
+++ examples/track_dropped.py
@@ -1,23 +1,22 @@
-import os
+"""
+track_dropped.py
+
+While developing fastprop, it became obvious that not all of the descriptors calculated by mordredcommunity
+would necessarily be useful. This script was where the pre-processing pipeline was initially developed
+(which now lives in `fastprop.preprocessing`) but also shows how to track which descriptors were dropped,
+used to develop the `optimized` subset of descriptors.
+"""
 import warnings
-from multiprocessing import Pool
-from time import perf_counter
 
 import numpy as np
 import pandas as pd
-import pytorch_lightning as pl
-import torch
-from astartes import train_val_test_split
 from mordred import Calculator, descriptors
-from pytorch_lightning import LightningDataModule
-from pytorch_lightning.loggers import CSVLogger, TensorBoardLogger
 from rdkit import Chem
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import MinMaxScaler
-from torch.utils.data import Dataset as TorchDataset
-from torchmetrics.functional import mean_absolute_percentage_error as mape
 
+# https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/qm8.csv
 data = pd.read_csv("qm8.csv")
 
 targets = data["E1-PBE0"].to_numpy()
@@ -36,24 +35,18 @@ with warnings.catch_warnings():
     cleaned_mordred_descs = imp_mean.fit_transform(mordred_descs, targets)
 
 print(cleaned_mordred_descs.shape)
-# scale each column 0-1
-# feature_scaler = MinMaxScaler()
-# cleaned_normalized_mordred_descs = feature_scaler.fit_transform(cleaned_mordred_descs, targets)
-# print("size after clean (drop empty, impute missing, scale 0-1):", cleaned_normalized_mordred_descs.shape)
 
 # drop low variance features
 X = VarianceThreshold(threshold=0).set_output(transform="pandas").fit_transform(cleaned_mordred_descs, y)
 print("size after invariant feature removal:", X.shape)
 
-threshold=0.95
+threshold = 0.95
 df_corr = X.corr().abs()
 upper = df_corr.where(np.triu(np.ones(df_corr.shape), k=1).astype(bool))
 to_drop = [column for column in upper.columns if any(upper[column] > threshold)]
 df = X.drop(to_drop, axis=1)
 print(df.shape)
-# print(df.columns)
 
-# print(X)
 with open("out_less.txt", "w") as file:
     for desc in df.columns:
         file.write(desc + "\n")
